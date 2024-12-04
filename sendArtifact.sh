14cf39c5cccde03cacc413ce7aa10c4811d35b39
destroyer <destroyer.dcf@gmail.com>
2024-12-04 15:47:38 +0100

    changes compiler scripts

--- sendArtifact.sh
+++ sendArtifact.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export PICO_SDK_PATH=~/pico/pico-sdk
+OPENOCD_PATH=/home/destroyer/Documents/openocd
+OPENOCD_BIN=$OPENOCD_PATH/src/openocd
 
-/home/destroyer/Documents/openocd/src/openocd -f /home/destroyer/Documents/openocd/tcl/interface/cmsis-dap.cfg -f /home/destroyer/Documents/openocd/tcl/target/rp2040.cfg -c "adapter speed 5000" -c "program build/usb2ps2.elf verify reset exit"
+"$OPENOCD_BIN" -f $OPENOCD_PATH/tcl/interface/cmsis-dap.cfg -f $OPENOCD_PATH/tcl/target/rp2040.cfg -c "adapter speed 5000" -c "program build/usb2ps2.elf verify reset exit"
